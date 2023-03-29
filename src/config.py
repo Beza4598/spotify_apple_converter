0fe28ba6828c010f635e63f701fa173232f6e29c
Beza4598 <techgeniusvideos@gmail.com>
2023-03-29 03:35:57 -0400

    lint

--- src/config.py
+++ src/config.py
@@ -7,7 +7,12 @@ ThkLZ+iA
 -----END PRIVATE KEY-----"""
 key_id = "UNU5V5VXP9"
 team_id = "XWV97XL492"
-music_user_token = "AhX0j0IoaImAAUYwyHw+BojoFx9tvZu2Vby4n8IdoWvWpk1JIBQdGju7Qvd0L5gcZRHKX4uqaf8XIIRUd4NXhbqrXaSEBM2TEUE0DjmlwL67DL3Ezst2Oa1o924g2PugILfXOpVBY8wNavOK8LTPEZ3LjVaFD/BkRzUEVAZ634y6eNr3dV0z3kI8ZazmPDGelaPD6ylAM+fDZkUmoWy2Ee5VU5pvKtgp4QibQfWoHBSrBaWzhw=="
+music_user_token = (
+    "AhX0j0IoaImAAUYwyHw+BojoFx9tvZu2Vby4n8IdoWvWpk1JIBQdGju7Qvd0L5gc"
+    "ZRHKX4uqaf8XIIRUd4NXhbqrXaSEBM2TEUE0DjmlwL67DL3Ezst2Oa1o924g2Pug"
+    "ILfXOpVBY8wNavOK8LTPEZ3LjVaFD/BkRzUEVAZ634y6eNr3dV0z3kI8ZazmPDGe"
+    "laPD6ylAM+fDZkUmoWy2Ee5VU5pvKtgp4QibQfWoHBSrBaWzhw=="
+)
 
 # Spotify api confugration
 client_id = "90cc35748b224a06b666072fe72b93e1"
--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,6 @@ from unittest.mock import patch
 from src.spotify_client import SpotifyClient
 from unittest.mock import MagicMock
 from io import StringIO
-from contextlib import redirect_stdout
 
 
 class TestSpotifyClient(unittest.TestCase):
@@ -65,15 +64,16 @@ class TestSpotifyClient(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
     def test_spotifyToAppleMusicUsingISRC(self):
-
         client = SpotifyClient("testuser", transfer_all=False)
-        tracks = pd.DataFrame({
-            "track_ids": ["1", "2"],
-            "track_titles": ["Track 1", "Track 2"],
-            "track_first_artists": ["Artist 1", "Artist 2"],
-            "track_popularity": [80, 90],
-            "track_isrc": ["ISRC1", "ISRC2"],
-        })
+        tracks = pd.DataFrame(
+            {
+                "track_ids": ["1", "2"],
+                "track_titles": ["Track 1", "Track 2"],
+                "track_first_artists": ["Artist 1", "Artist 2"],
+                "track_popularity": [80, 90],
+                "track_isrc": ["ISRC1", "ISRC2"],
+            }
+        )
         with patch('applemusicpy.AppleMusic.songs_by_isrc', return_value={"data": []}):
             track_dataframe = client.spotifyToAppleMusicUsingISRC(tracks)
 
@@ -98,38 +98,6 @@ class TestSpotifyClient(unittest.TestCase):
 
         self.assertTrue(result)
 
-    # @patch('spotify_client.SpotifyClient.get_user_playlists_sp')
-    # @patch('spotify_client.SpotifyClient.get_tracks_for_playlist')
-    # @patch('spotify_client.SpotifyClient.spotifyToAppleMusicUsingISRC')
-    # @patch('spotify_client.SpotifyClient.create_new_playlist')
-    # @patch('spotify_client.SpotifyClient.insert_track_to_playlist')
-    # def test_transfer_all_playlists_integration(self, mock_insert_track, mock_create_playlist, mock_spotify_to_apple_music, mock_get_tracks, mock_get_playlists):
-        
-    #     client = SpotifyClient("testuser", transfer_all=False)
-
-    #     mock_get_playlists.return_value = pd.DataFrame({"playlist_id": ["playlist_1"], "playlist_name": ["Playlist 1"]})
-    #     mock_get_tracks.return_value = pd.DataFrame(
-    #         {
-    #             "track_ids": ["1"],
-    #             "track_titles": ["Track 1"],
-    #             "track_first_artists": ["Artist 1"],
-    #             "track_popularity": [80],
-    #             "track_isrc": ["ISRC1"],
-    #         }
-    #     )
-    #     mock_spotify_to_apple_music.return_value = pd.DataFrame({"track_name": ["Track 1"], "id": ["AM1"]})
-    #     mock_create_playlist.return_value = 'new_playlist_id'
-    #     mock_insert_track.return_value = True
-
-    #     # Redirect stdout to capture print statements
-    #     captured_output = StringIO()
-    #     with redirect_stdout(captured_output):
-    #         client.transfer_all_playlists(mock_get_playlists.return_value)
-
-    #     output = captured_output.getvalue().split('\n')
-    #     self.assertIn('Adding songs to playlist new_playlist_id', output)
-    #     self.assertIn('Added track Track 1', output)
-    #     self.assertIn('Here is a link to the apple music playlist created: https://music.apple.com/library/playlist/new_playlist_id', output)
 
 if __name__ == "__main__":
     unittest.main()
