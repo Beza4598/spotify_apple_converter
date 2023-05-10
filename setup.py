d3e5864e357769f8647d41235c8d5af0a882f011
Beza4598 <techgeniusvideos@gmail.com>
2023-05-10 17:55:12 -0400

    version update

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="spotify-to-apple-py",
+      name="spotify-to-apple-py",
     url="https://github.com/Beza4598/spotify_apple_converter.git",
+    long_description_content_type="text/markdown",
+    long_description="README",
     author="Beza Amsalu",
     author_email="baa2159@columbia.edu",
-    version="0.1.2"
+    version="0.2.0",
 )
